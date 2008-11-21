eb1cb6dac7be7c00fe50f9a0c587d9768ffebd6f
Antony Dovgal <tony@daylessday.org>
2008-11-22 00:23:42 +0300

    unify EOL for additional consistency

--- src/hpdf_dict.c
+++ src/hpdf_dict.c
@@ -223,7 +223,7 @@ HPDF_Dict_Write  (HPDF_Dict     dict,
                     0);
         }
 
-        if ((ret = HPDF_Stream_WriteStr (stream, "\012stream\015\012"))
+        if ((ret = HPDF_Stream_WriteStr (stream, "\012stream\012"))
                 != HPDF_OK)
             return ret;
 
