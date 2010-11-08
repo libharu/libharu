a814db3e4953917ccaff08a4ffea2e4cd7260e79
Antony Dovgal <tony@daylessday.org>
2010-11-08 18:48:23 +0300

    add missing typedefs (patch by Andrea D'Amore) and prepare for 2.2.1

--- include/hpdf.h
+++ include/hpdf.h
@@ -66,6 +66,12 @@ typedef HPDF_HANDLE   HPDF_Annotation;
 typedef HPDF_HANDLE   HPDF_ExtGState;
 typedef HPDF_HANDLE   HPDF_FontDef;
 typedef HPDF_HANDLE   HPDF_U3D;
+typedef HPDF_HANDLE   HPDF_Error;
+typedef HPDF_HANDLE   HPDF_MMgr;
+typedef HPDF_HANDLE   HPDF_Dict;
+typedef HPDF_HANDLE   HPDF_EmbeddedFile;
+typedef HPDF_HANDLE   HPDF_OutputIntent;
+typedef HPDF_HANDLE   HPDF_Xref;
 
 #else
 
--- include/hpdf_version.h
+++ include/hpdf_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define HPDF_MAJOR_VERSION 2
 #define HPDF_MINOR_VERSION 2
-#define HPDF_BUGFIX_VERSION 0
-#define HPDF_EXTRA_VERSION ""
-#define HPDF_VERSION_TEXT "2.2.0"
-#define HPDF_VERSION_ID 20200
+#define HPDF_BUGFIX_VERSION 1
+#define HPDF_EXTRA_VERSION "-dev"
+#define HPDF_VERSION_TEXT "2.2.1-dev"
+#define HPDF_VERSION_ID 20201
