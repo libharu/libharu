6ffdbc09a4811160c185892a9b5255e14b653a6e
Antony Dovgal <tony@daylessday.org>
2010-11-22 13:13:38 +0300

    -dev

--- include/hpdf_version.h
+++ include/hpdf_version.h
@@ -2,7 +2,7 @@
 /* edit configure.in to change version number */
 #define HPDF_MAJOR_VERSION 2
 #define HPDF_MINOR_VERSION 2
-#define HPDF_BUGFIX_VERSION 1
+#define HPDF_BUGFIX_VERSION 2
 #define HPDF_EXTRA_VERSION "-dev"
-#define HPDF_VERSION_TEXT "2.2.1-dev"
-#define HPDF_VERSION_ID 20201
+#define HPDF_VERSION_TEXT "2.2.2-dev"
+#define HPDF_VERSION_ID 20202
