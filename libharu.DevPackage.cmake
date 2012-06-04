ae265f994be5c7ac5d72152be610386342a29519
Paul Harris <paulharris@computer.org>
2012-06-04 12:49:48 +0800

    Fixed problems with cmake files
    Problems included:
     - Was using LIBHARU_* instead of LIBHPDF_*
     - Was using a add_definitions() hack for -DLIBHPDF_HAVE_NOPNGLIB,
       rather than using define flags in the generated config files.
     - Generated config files were not correctly importing the cmake variables.
     - Added extra HAVE_MEMORY_H and friend detection routines,
       to match what automake does.

--- libharu.DevPackage.cmake
+++ libharu.DevPackage.cmake
@@ -1,10 +1,10 @@
 [Setup]
 Version=2
 AppName=libHaru
-AppVerName=libHaru Version ${LIBHARU_VERSION}
-AppVersion=${LIBHARU_VERSION}
-MenuName=libHaru ${LIBHARU_VERSION}
-Description=${LIBHARU_DESCRIPTION}
+AppVerName=libHaru Version ${LIBHPDF_VERSION}
+AppVersion=${LIBHPDF_VERSION}
+MenuName=libHaru ${LIBHPDF_VERSION}
+Description=${LIBHPDF_DESCRIPTION}
 Url=http://www.libharu.org
 Readme=README
 Dependencies=zlib libpng
