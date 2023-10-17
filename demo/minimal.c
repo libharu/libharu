6fc6bbd124028144c8baeba885ef5cf083cfd70e
Dmitry Solomennikov <dmitrys99@mail.ru>
2023-10-17 21:30:19 +0300

    Add minimal.c sample program
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

---
+++ demo/minimal.c
@@ -0,0 +1,62 @@
+/*
+ * << Haru Free PDF Library 2.4.4 >> -- minimal.c
+ *
+ * Copyright (c) 1999-2006 Takeshi Kanno <takeshi_kanno@est.hi-ho.ne.jp>
+ * Copyright (c) 2023 Dmitry Solomennikov
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.
+ * It is provided "as is" without express or implied warranty.
+ *
+ */
+
+/**
+  \par Minimal HPDF program demo
+
+  This is bare minimum program, creating PDF document.
+  It creates \ref HPDF_Doc object, adds \ref HPDF_Page object into it,
+  sets added page size and orientation and saves document to a file.
+
+  Last action is to clean up everything.
+*/ 
+#include <stdio.h>
+#include <setjmp.h>
+#include "hpdf.h"
+#include "handler.h"
+
+int
+main (int argc, char **argv)
+{
+    HPDF_Doc  pdf;
+    HPDF_Page page;
+    char fname[11] = "minimal.pdf";
+
+    /* create document object*/
+    pdf = HPDF_New (demo_error_handler, NULL);
+    if (!pdf) {
+        printf ("error: cannot create PdfDoc object\n");
+        return 1;
+    }
+
+    if (setjmp(env)) {
+        HPDF_Free (pdf);
+        return 1;
+    }
+
+    /* add a new page object. */
+    page = HPDF_AddPage (pdf);
+
+    /* set up added page */
+    HPDF_Page_SetSize (page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_LANDSCAPE);
+
+    /* save document to a file */
+    HPDF_SaveToFile (pdf, fname);
+
+    /* clean up */
+    HPDF_Free (pdf);
+
+    return 0;
+}
